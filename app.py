5c620581250e6c8e51e6e24be352fa57631a0cec
nagoyuta <yuta.nago@nahato.co.jp>
2024-08-01 17:23:24 +0900

    strftime復活

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def get_data(days, tickers):
     for company in tickers.keys():
         tkr = yf.Ticker(tickers[company])
         hist = tkr.history(period=f'{days}d')
-        # hist.index = hist.index.strftime('%d %B %Y')
+        hist.index = hist.index.strftime('%d %B %Y')
         # hist.index = hist.index.to_series().apply(lambda x: x.strftime('%d %B %Y'))
         hist = hist[['Close']]
         hist.columns = [company]
